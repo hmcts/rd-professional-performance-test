19c94b10201a305384d8585416ed0f61a1e142e3
jmcadam <mcadam88@gmail.com>
2024-02-20 12:39:59 +0000

    update gradle version & internal add user payload

--- src/gatling/simulations/scenarios/internal/Internal_AddInternalUserToOrg.scala
+++ src/gatling/simulations/scenarios/internal/Internal_AddInternalUserToOrg.scala
@@ -14,33 +14,25 @@ object Internal_AddInternalUserToOrg {
   private def internalUser_lastName(): String = rng.alphanumeric.take(20).mkString
 
   val config: Config = ConfigFactory.load()
-
   val OrgIdData = csv("prdIntOrgIDs.csv").circular
-
-  val addInternalUserString = "{\n \"firstName\": \"#{userFirstname}\",\n \"lastName\": \"#{userLastname}\",\n \"email\": \"#{userEmail}\",\n \"roles\": [\n   \"pui-user-manager\",\n   \"pui-organisation-manager\"\n ]\n,\n        \"jurisdictions\": [\n    {\n      \"id\": \"Divorce\"\n    },\n    {\n      \"id\": \"SSCS\"\n    },\n    {\n      \"id\": \"Probate\"\n    },\n    {\n      \"id\": \"Public Law\"\n    },\n    {\n      \"id\": \"Bulk Scanning\"\n    },\n    {\n      \"id\": \"Immigration & Asylum\"\n    },\n    {\n      \"id\": \"Civil Money Claims\"\n    },\n    {\n      \"id\": \"Employment\"\n    },\n    {\n      \"id\": \"Family public law and adoption\"\n    },\n    {\n      \"id\": \"Civil enforcement and possession\"\n    }\n  ]\n}"
-
-  // val AddIntUsrMin = config.getString("internal.addIntUsrMin").toInt
-  // val AddIntUsrMax = config.getString("internal.addIntUsrMax").toInt
 
   val AddInternalUserToOrg = 
   
-    repeat(1){
-      exec(_.setAll(
-          ("InternalUser_FirstName",internalUser_firstName()),
-          ("InternalUser_LastName",internalUser_lastName())
-        ))
+    exec(_.setAll(
+        ("InternalUser_FirstName",internalUser_firstName()),
+        ("InternalUser_LastName",internalUser_lastName())
+      ))
 
-      .feed(OrgIdData)
+    .feed(OrgIdData)
 
-      .exec(http("RD06_Internal_AddInternalUserToOrganisation")
-        .post("/refdata/internal/v1/organisations/#{NewPendingOrg_Id}/users/")
-        .header("Authorization", "Bearer #{accessToken}")
-        .header("ServiceAuthorization", "Bearer #{s2sToken}")
-        .body(StringBody(addInternalUserString))
-        .header("Content-Type", "application/json")
-        .check(status is 201)
-        .check(jsonPath("$.userIdentifier").saveAs("userId")))
+    .exec(http("RD06_Internal_AddInternalUserToOrganisation")
+      .post("/refdata/internal/v1/organisations/#{NewPendingOrg_Id}/users/")
+      .header("Authorization", "Bearer #{accessToken}")
+      .header("ServiceAuthorization", "Bearer #{s2sToken}")
+      .body(ElFileBody("bodies/internal/AddInternalUser.json"))
+      .header("Content-Type", "application/json")
+      .check(status is 201)
+      .check(jsonPath("$.userIdentifier").saveAs("userId")))
 
-      .pause(Environment.thinkTime)
-    }
+    .pause(Environment.thinkTime)
 }
